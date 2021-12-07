9bfd7b3a4924102b38a9ae5ee5a5a8ae6c6af7cf
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-06 19:09:21 -0800

    Add files via upload

---
+++ Projectiles/MiscPostSentinel/TheTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using AncientSandsMod.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientSandsMod.Projectiles.MiscPostSentinel
+{
+    public class TheTable : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+
+            projectile.width = 30;
+            projectile.height = 30;
+            projectile.aiStyle = 3;
+            projectile.friendly = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 600;
+            projectile.alpha = 0;
+            projectile.extraUpdates = 1;
+            
+
+
+
+        }
+
+        public override void AI()
+        {
+            Lighting.AddLight(projectile.position, 0.5f, 0.5f, 0.5f);
+
+
+           
+    
+        }
+
+        
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            projectile.Kill();
+
+            projectile.soundDelay--;
+            if (projectile.soundDelay <= 0)
+            {
+                Main.PlaySound(SoundID.Shatter, (int)projectile.Center.X, (int)projectile.Center.Y, 1);
+                projectile.soundDelay = 45;
+
+            }
+
+
+
+            
+        }
+
+
+
+
+
+
+
+    }
+}
