f1abb505f3b5e662f16946a114076f5862a3dd8c
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-07 17:29:36 -0800

    Add files via upload

---
+++ Items/VanillaItems/HermesBoots.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items
+{
+	// This file shows a very simple example of a GlobalItem class. GlobalItem hooks are called on all items in the game and are suitable for sweeping changes like 
+	// adding additional data to all items in the game. Here we simply adjust the damage of the Copper Shortsword item, as it is simple to understand. 
+	// See other GlobalItem classes in ExampleMod to see other ways that GlobalItem can be used.
+	public class HermesBoots : GlobalItem
+	{
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.OldShoe, 2);
+			recipe.AddIngredient(ItemID.FallenStar, 5);
+			recipe.AddIngredient(ItemID.Leather, 5);
+			recipe.AddIngredient(ItemID.Emerald, 3);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(ItemID.HermesBoots);
+			recipe.AddRecipe();
+
+			
+		}
+
+		
+
+
+
+	}
+}
---
+++ Items/VanillaItems/Leather.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Items
+{
+	// This file shows a very simple example of a GlobalItem class. GlobalItem hooks are called on all items in the game and are suitable for sweeping changes like 
+	// adding additional data to all items in the game. Here we simply adjust the damage of the Copper Shortsword item, as it is simple to understand. 
+	// See other GlobalItem classes in ExampleMod to see other ways that GlobalItem can be used.
+	public class Leather : GlobalItem
+	{
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Vertebrae, 5);
+
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(ItemID.Leather);
+			recipe.AddRecipe();
+
+
+		}
+
+
+
+
+
+	}
+}
