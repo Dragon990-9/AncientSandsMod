1378b10ff45b3fab07c37c475c8a7dd3117503b9
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-07 17:30:17 -0800

    Add files via upload

---
+++ Items/Tools/AncientPickaxe.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientSandsMod.Items.Tools
+{
+	public class AncientPickaxe : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Can mine bluesteel");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 5;
+			item.melee = true;
+			item.width = 42;
+			item.height = 42;
+			item.useTime = 17;
+			item.useAnimation = 17;
+			item.pick = 61;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 6;
+			item.value = 10254;
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Materials.AncientMetalPlating>(), 12);
+			recipe.AddIngredient(ModContent.ItemType<Materials.AncientEnergy>(), 6);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		/*public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.NextBool(10))
+			{
+				int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, ModContent.DustType<Sparkle>());
+			}
+		}*/
+	}
+}
