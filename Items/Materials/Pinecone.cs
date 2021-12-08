5889462aeb5bdcb91f28930e5cdee84ca13e9dcd
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-07 20:57:45 -0800

    Add files via upload

---
+++ Items/Materials/Pinecone.cs
@@ -0,0 +1,38 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using AncientSandsMod.Tiles;
+using AncientSandsMod.Items.Placeable;
+
+namespace AncientSandsMod.Items.Materials
+{
+	public class Pinecone : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Pinecone"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 20;
+			item.height = 20;
+			item.maxStack = 999;
+			item.value = 1000;
+			item.rare = ItemRarityID.White;
+
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BorealWood, 5);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+
+		}
+
+		
+	}
+}
