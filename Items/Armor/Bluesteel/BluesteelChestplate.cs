4d93867a9d42770a893fe67cad132c637f491627
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-07 20:57:18 -0800

    Add files via upload

---
+++ Items/Armor/Bluesteel/BluesteelChestplate.cs
@@ -0,0 +1,43 @@
+﻿
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientSandsMod.Items.Armor.Bluesteel
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class BluesteelChestplate : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Bluesteel Chestplate");
+			
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 18;
+			item.height = 18;
+			item.value = 20000;
+			item.rare = ItemRarityID.White;
+			item.defense = 5;
+		}
+
+		/*public override void UpdateEquip(Player player)
+		{
+			player.buffImmune[BuffID.OnFire] = true;
+			player.statManaMax2 += 20;
+			player.maxMinions++;
+		}*/
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Materials.BluesteelBar>(), 25);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/Armor/Bluesteel/BluesteelLeggings.cs
@@ -0,0 +1,39 @@
+﻿
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace AncientSandsMod.Items.Armor.Bluesteel
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class BluesteelLeggings : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("5% more walk speed");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 18;
+			item.height = 18;
+			item.value = 20000;
+			item.rare = ItemRarityID.White;
+			item.defense = 4;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.moveSpeed += 0.05f;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Materials.BluesteelBar>(), 18);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
