bbd4f3ebff5b4a4e568f37d977ec843dfb555910
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-07 21:01:36 -0800

    Add files via upload

---
+++ Projectiles/MiscPostSentinel/Pinecone.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using AncientSandsMod.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientSandsMod.Projectiles.MiscPostSentinel
+{
+    public class Pinecone : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+
+            projectile.width = 28;
+            projectile.height = 32;
+            projectile.friendly = true;
+            projectile.aiStyle = 1;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 600;
+            projectile.alpha = 0;
+            projectile.extraUpdates = 1;
+            projectile.scale = 0.5f;
+
+
+
+
+        }
+
+        public override void AI()
+        {
+            if (projectile.timeLeft < 550)
+            {
+                projectile.aiStyle = 2;
+
+            }
+          
+
+
+        }
+
+
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            projectile.Kill();
+
+            projectile.soundDelay--;
+            if (projectile.soundDelay <= 0)
+            {
+                Main.PlaySound(SoundID.Dig, (int)projectile.Center.X, (int)projectile.Center.Y, 1);
+                projectile.soundDelay = 45;
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
