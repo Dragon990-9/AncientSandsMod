a2264ba85179370164fe0065d146668f408d865e
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-08 20:17:55 -0800

    Add files via upload

---
+++ Items/Materials/MoonDust.cs
@@ -0,0 +1,37 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+
+
+namespace AncientSandsMod.Items.Materials
+{
+	public class MoonDust : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Moon Dust"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("");
+
+	
+			//ItemID.Sets.ItemNoGravity[item.type] = true;
+
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 24;
+			item.height = 24;
+			item.maxStack = 999;
+			item.value = 100;
+			item.rare = ItemRarityID.White;
+
+		}
+
+		public override void AddRecipes()
+		{
+
+		}
+	}
+}
