2782385b5a78c9076fc773d5f13e28b816a0b650
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-08 20:15:39 -0800

    Add files via upload

---
+++ Items/Weapons/PreBoss/OasisBlade.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientSandsMod.Items.Weapons.PreBoss
+{
+	public class OasisBlade : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Oasis Blade"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 7;
+			item.melee = true;
+			item.width = 44;
+			item.height = 46;
+			item.useTime = 35;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 2;
+			item.value = 10535;
+			item.rare = ItemRarityID.White;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.scale = 1;
+			item.shootSpeed = 10f;
+
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.SandBlock, 15);
+			recipe.AddIngredient(ItemID.Waterleaf, 1);
+			recipe.AddIngredient(ItemID.PalmWood, 10);
+
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		
+	}
+}
---
+++ Items/Weapons/PreBoss/WoodenDart.cs
@@ -0,0 +1,46 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientSandsMod.Items.Weapons.PreBoss
+{
+	public class WoodenDart : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Wooden Dart"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 3;
+			item.thrown = true;
+			item.width = 10;
+			item.height = 24;
+			item.useTime = 21;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 1;
+			item.value = 345;
+			item.rare = ItemRarityID.White;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.scale = 1;
+			item.shoot = ModContent.ProjectileType<Projectiles.PreBoss.WoodenDart>();
+			item.shootSpeed = 5f;
+			item.maxStack = 999;
+			item.consumable = true;
+
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 3);
+			recipe.anyWood = true;
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this, 5);
+			recipe.AddRecipe();
+		}
+	}
+}
