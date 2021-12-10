d60e60f321f176200b9204404fa880406c2a2e77
Dragon990-9 <65734777+Dragon990-9@users.noreply.github.com>
2021-12-09 19:04:31 -0800

    Add files via upload

---
+++ Projectiles/PreBoss/SeaUrchin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AncientSandsMod.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AncientSandsMod.Projectiles.PreBoss
+{
+    public class SeaUrchin : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.aiStyle = 2;
+            projectile.friendly = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 500;
+            projectile.alpha = 0;
+            projectile.extraUpdates = 1;
+
+
+
+        }
+
+
+
+            
+        }
+
+      
+
+
+
+    }
